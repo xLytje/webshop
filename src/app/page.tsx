6b31a3e0fbf7f4f8245f5cdc2260391eba73ce2d
xLytje <rasmuslytje@gmail.com>
2023-10-30 17:45:09 +0100

    setup changes

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import Link from 'next/link';
+ 
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52" />
+    </main>
+  );
+}
